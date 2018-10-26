be4d90307a449691c0cb9ed3836acec999c7b862
Paula H <42469219+PaulaHolubek@users.noreply.github.com>
2018-10-25 21:18:18 -0400

    Add files via upload

---
+++ CollectionSort/Controllers/NumsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CollectionSort.Controllers
+{
+    public class NumsController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            List<int> numbers = new List<int>();
+            Random rnd = new Random();
+
+            for (int counter = 1; counter <= 5; counter++)
+            {                
+                int num = rnd.Next(50);
+                numbers.Add(num);
+            }
+
+            numbers.Sort();
+
+            ViewBag.Numbers = numbers;          
+            return View();
+        }
+    }
+}
---
+++ CollectionSort/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CollectionSort.MvcApplication" Language="C#" %>
